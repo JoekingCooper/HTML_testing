5a007c991faae271b98c45c4c4a09e57ea18c389
JoekingCooper <joseph.cooper@beamly.com>
2019-01-23 13:49:06 +0000

    autoreporting p2 2

---
+++ autoreporting_p02_1.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from jinja2 import FileSystemLoader, Environment
+
+# Allow for very wide columns - otherwise columns are spaced and ellipse'd
+pd.set_option("display.max_colwidth", 200)
+
+
+def csv_to_html(filepath):
+    """
+    Open a .csv file and return it in HTML format.
+    :param filepath: Filepath to a .csv file to be read.
+    :return: String of HTML to be published.
+    """
+    df = pd.read_csv(filepath, index_col=0)
+    html = df.to_html()
+    return html
+
+
+# Configure Jinja and ready the loader
+env = Environment(
+    loader=FileSystemLoader(searchpath="templates")
+)
+
+# Assemble the templates we'll use
+base_template = env.get_template("report.html")
+table_section_template = env.get_template("table_section.html")
+
+
+def main():
+    """
+    Entry point for the script.
+    Render a template and write it to file.
+    :return:
+    """
+
+    # Content to be published
+    title = "Model Report"
+    sections = list()
+    sections.append(table_section_template.render(
+        model="VGG19",
+        dataset="VGG19_results.csv",
+        table=csv_to_html("datasets/VGG19_results.csv")
+    ))
+    sections.append(table_section_template.render(
+        model="MobileNet",
+        dataset="MobileNet_results.csv",
+        table=csv_to_html("datasets/MobileNet_results.csv")
+    ))
+
+    with open("outputs/report.html", "w") as f:
+            f.write(base_template.render(
+                title=title,
+                sections=sections
+            ))
+
+
+if __name__ == "__main__":
+    main()
